693a435f23d7d18204f0f30c5c9819ae813d3deb
Abhishek Velichala <70202321+AbhishekRaoV@users.noreply.github.com>
2024-07-16 05:58:28 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"  // Replace with your desired AWS region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-09e67e426f25ce0d7" // Ubuntu 20.04 LTS 64-bit AMI, you may need to change this for other regions
+  instance_type = "t2.medium"
+  key_name      = "nextgen-devops-team"     // Replace with your SSH key pair name
+  tags = {
+    Name = "auctane-demo"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.example.public_ip
+}
